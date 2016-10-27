fa85072855bf5f4454cd16b00e55f87bddfe997c
Patrick Heinze <pheinze82@gmail.com>
2016-10-27 14:29:54 +0000

    Aufgaben löschen und editieren zugefügt

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
+    
     def index
-        @items = Item.all.order("created_at DESC")
+        @items = Item.all.order("created_at DESC" )
     end
     
     def new
@@ -8,7 +10,6 @@ class ItemsController < ApplicationController
     end
     
     def create
-        @item = Item.new(item_params)
         if @item.save
             redirect_to root_path
         else 
@@ -16,8 +17,32 @@ class ItemsController < ApplicationController
         end
     end
     
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @item.update(item_params)
+            redirect_to root_path 
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @item.destroy
+        redirect_to root_path
+    end
     private
     def item_params
         params.require(:item).permit(:title, :description)
     end
+    
+    private
+    def find_item
+        @item = Item.find(params[:id])
+    end
+    
 end
